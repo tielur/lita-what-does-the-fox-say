2d483d633c7ccc803f213188684b537a828c54b7
Tyler Clemens <tyler.clemens@activenetwork.com>
2014-08-18 00:15:41 -0700

    add specs

--- spec/lita/handlers/what_does_the_fox_say_spec.rb
+++ spec/lita/handlers/what_does_the_fox_say_spec.rb
@@ -1,4 +1,13 @@
 require "spec_helper"
 
 describe Lita::Handlers::WhatDoesTheFoxSay, lita_handler: true do
+  it { routes_command("what does the fox say").to(:what_the_fox_says) }
+
+  describe '#what_the_fox_says' do
+    it 'responds  with what the fox says' do
+      send_command "what does the fox say"
+      expect(replies.first).to match(":dog: goes woof")
+      expect(replies.last).to match("What does the :wolf: say!?")
+    end
+  end
 end
